cd9eed32de41d99629e119be5b58bc5e5c9d1534
Hyungrok-Kim <cjfwlsdlvuswl@daum.net>
2021-02-08 17:01:11 +0900

    io_03

---
+++ test_io_03.py
@@ -0,0 +1,7 @@
+# -*- coding:utf-8 -*-
+
+with open('test01.txt', 'a') as file :
+    file.write('\nPen PineApple Apple Pen')
+
+    # JSON, MAP, List와 같은 객체를 저장하는 방법은 ?
+    # -> pickle이라는 모듈을 사용해야 함.
